55f68a1c59ec07eafb5f4fa7a9f4e5d93261d4b1
nedonebo21 <cursed.void10007@gmail.com>
2025-04-18 16:04:47 +0400

    example using flex-grow (header,content,footer)

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,23 @@ function App() {
     return (
         <div className="App">
 
-            <div className="flex-container">
-                <div className="myDiv">Im div</div>
-                <img className="myImg" src="https://www.sitedelkino.ru/images/rasdel_css/Flexbox2.jpg" alt=""/>
-                <input className="myInput" type="text" placeholder='Im Input'/>
-                <p className="myText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi, autem,
-                    beatae
-                    cumque dolorem dolorum enim eveniet excepturi magni non obcaecati perferendis ratione rem
-                    reprehenderit
-                    sequi ut vel vero.</p>
-                <span className="mySpan">Im span</span>
+            <div className="wrapper">
+                <header>Header</header>
+                <main>Content</main>
+                <footer>Footer</footer>
             </div>
+
+            {/*<div className="flex-container">*/}
+            {/*    <div className="myDiv">Im div</div>*/}
+            {/*    <img className="myImg" src="https://www.sitedelkino.ru/images/rasdel_css/Flexbox2.jpg" alt=""/>*/}
+            {/*    <input className="myInput" type="text" placeholder='Im Input'/>*/}
+            {/*    <p className="myText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi, autem,*/}
+            {/*        beatae*/}
+            {/*        cumque dolorem dolorum enim eveniet excepturi magni non obcaecati perferendis ratione rem*/}
+            {/*        reprehenderit*/}
+            {/*        sequi ut vel vero.</p>*/}
+            {/*    <span className="mySpan">Im span</span>*/}
+            {/*</div>*/}
 
         </div>
     )
