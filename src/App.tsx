886331f9b47c9b9498cb18b501f597c1da53876b
nedonebo21 <cursed.void10007@gmail.com>
2025-04-18 14:59:56 +0400

    add flex container html

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,19 @@ function App() {
 
     return (
         <div className="App">
-            <div className="myDiv">Im div</div>
-            <img className="myImg" src="https://www.sitedelkino.ru/images/rasdel_css/Flexbox2.jpg" alt=""/>
-            <input className="myInput" type="text" placeholder='Im Input'/>
-            <p className="myText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi, autem, beatae
-                cumque dolorem dolorum enim eveniet excepturi magni non obcaecati perferendis ratione rem reprehenderit
-                sequi ut vel vero.</p>
-            <span className="mySpan">Im span</span>
+
+            <div className="flex-container">
+                <div className="myDiv">Im div</div>
+                <img className="myImg" src="https://www.sitedelkino.ru/images/rasdel_css/Flexbox2.jpg" alt=""/>
+                <input className="myInput" type="text" placeholder='Im Input'/>
+                <p className="myText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi, autem,
+                    beatae
+                    cumque dolorem dolorum enim eveniet excepturi magni non obcaecati perferendis ratione rem
+                    reprehenderit
+                    sequi ut vel vero.</p>
+                <span className="mySpan">Im span</span>
+            </div>
+
         </div>
     )
 }
