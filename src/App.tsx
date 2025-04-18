953129fc85bf3024d4d23f8ca0d40909b087bb3b
nedonebo21 <cursed.void10007@gmail.com>
2025-04-18 14:56:17 +0400

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="App">
+            <div className="myDiv">Im div</div>
+            <img className="myImg" src="https://www.sitedelkino.ru/images/rasdel_css/Flexbox2.jpg" alt=""/>
+            <input className="myInput" type="text" placeholder='Im Input'/>
+            <p className="myText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi, autem, beatae
+                cumque dolorem dolorum enim eveniet excepturi magni non obcaecati perferendis ratione rem reprehenderit
+                sequi ut vel vero.</p>
+            <span className="mySpan">Im span</span>
+        </div>
+    )
+}
+
+export default App
